2acb075baa47b376b67027505a9b51fe8d4e4c6e
Collin Jilbert <cjilbert504@gmail.com>
2019-11-19 21:03:51 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -20,12 +20,46 @@ def grade(grade)
 end
 end
 
+<<<<<<< HEAD
 def sort
 @roster.each do |key, value|
 @roster[key] = value.sort
 end
 @roster
 end
+=======
+class School
+
+attr_reader :name, :roster, :add_student
+
+def initialize(name)
+@name = name
+@roster = {}
+end
+
+def add_student(name, grade)
+@student_name = name
+@student_grade = grade
+@roster[@student_grade] ||= []; @roster[@student_grade] << @student_name
+end
+
+def grade(grade)
+@key = grade
+@roster[@key].collect do |students|
+ students
+end
+end
+
+def sort
+@roster.each do |key, value|
+@roster = value.sort
+@roster
+end
+end
+
+
+end
+>>>>>>> 7b48b1639104bc295a769f39888ec9863fcec462
 
 
 end
